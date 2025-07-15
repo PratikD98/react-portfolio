f3c5225d4071208765a3073e511e559da88793a3
Jagannath Deshmukh <p.jagannath.deshmukh@accenture.com>
2025-07-15 20:59:01 +0530

    Fix: Social icons not showing on Netlify

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import "../styles/sociallinks.css"; 
+import "../styles/sociallinks.css";
+import GitHubIcon from "../assets/icons/github.svg"; 
+import LinkedInIcon from "../assets/icons/linkedin.svg";
+import Twitter from "../assets/icons/twitter-x.svg";
 
 const SocialLinks = () => {
   return (
     <div className="social-icons">
       <a href="https://github.com/PratikD98" target="_blank" rel="noreferrer">
-        <img src="src/assets/icons/github.svg" alt="GitHub" />
+        <img src={GitHubIcon} alt="GitHub" />
       </a>
       <a href="https://www.linkedin.com/in/pratik-deshmukh98/" target="_blank" rel="noreferrer">
-        <img src="src/assets/icons/linkedin.svg" alt="LinkedIn" />
+        <img src={LinkedInIcon} alt="LinkedIn" />
       </a>
       <a href="https://x.com/PratikD70952346" target="_blank" rel="noreferrer">
         <img src="src/assets/icons/twitter-x.svg" alt="X (Twitter)" />
+        <img src={Twitter} alt="X (Twitter)" />
       </a>
       {/* <a href="https://stackoverflow.com/" target="_blank" rel="noreferrer">
         <img src="src/assets/icons/stack-overflow.svg" alt="Stack Overflow" />
